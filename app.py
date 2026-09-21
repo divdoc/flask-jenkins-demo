f15c7c7190e9154e64f98383860c85226f2c21a6
Admin <divyansha.damaniya@gmail.com>
2026-09-21 13:43:01 +0530

    Added hello endpoint

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
+from flask import Flask
+
+# Initialize the app FIRST
+app = Flask(__name__)
+
+
+# THEN define your routes
 @app.route("/hello")
 def hello():
-    return "Hello from Jenkins!"
+    return "Hello, World!"
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
